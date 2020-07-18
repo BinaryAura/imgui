5e6772c15e0ec54f7d54c55970d1c9a4ded00cf8
BinaryAura <jadunker@hotmail.com>
2020-07-18 14:09:00 -0500

    Added Premake5 file

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project("imgui")
+    kind "StaticLib"
+    language "C"
+
+    pic "On"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/build/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "imgui.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_widgets.cpp",
+        "imgui.h",
+        "imconfig.h",
+        "imgui_internal.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+    }
+    
+    includedirs {
+        "include"
+    }
+
+    filter "system:linux"
+        systemversion "latest"
+        staticruntime "On"
